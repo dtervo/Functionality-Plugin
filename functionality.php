80002ba49f12c00845bff39924a058ea9268e34b
dtervo <dan@tervosystems.com>
2014-03-29 08:39:21 -0400

    Initial Commit

---
+++ functionality.php
@@ -0,0 +1,78 @@
+<?php
+/* 
+Plugin Name: Tervo Systems Functionality Plugin
+Description: Custom functionality for Tervo Systems Websites
+Version: 0.1
+License: GPL
+Author: Dan Tervo
+Author URI: www.tervosystems.com
+*/
+
+// Add excerpt functionality to pages
+add_action( 'init', 'my_add_excerpts_to_pages' );
+function my_add_excerpts_to_pages() {
+     add_post_type_support( 'page', 'excerpt' );
+}
+
+//function to call first uploaded image in functions file
+function main_image() {
+$files = get_children('post_parent='.get_the_ID().'&post_type=attachment
+&post_mime_type=image&order=desc');
+  if($files) :
+    $keys = array_reverse(array_keys($files));
+    $j=0;
+    $num = $keys[$j];
+    $image=wp_get_attachment_image($num, 'large', true);
+    $imagepieces = explode('"', $image);
+    $imagepath = $imagepieces[1];
+    $main=wp_get_attachment_url($num);
+		$template=get_template_directory();
+		$the_title=get_the_title();
+    print "<img src='$main' alt='$the_title' class='alignleftthumb' />";
+  endif;
+/*				Add the following code to the index.php file to display thumbnails  								*/
+/*					<section class="post_content clearfix">															*/
+/*							<a href="<?php the_permalink() ?>" >													*/
+/*							<?php if (  (function_exists('has_post_thumbnail')) && (has_post_thumbnail())  ) {		*/
+/*							echo get_the_post_thumbnail($post_id, 'thumbnail', array('class' => 'alignleftthumb'));	*/
+/*							} else {																				*/
+/*							echo main_image();																		*/
+/*							} ?></a>																				*/
+/*							<?php the_excerpt('Read more &raquo;'); ?>												*/
+/*																													*/
+/*						</section> 																					*/
+}
+
+// Create copyright dates
+function tervo_copyright() {
+	global $wpdb;
+	$copyright_dates = $wpdb->get_results("
+		SELECT
+		YEAR(min(post_date_gmt)) AS firstdate,
+		YEAR(max(post_date_gmt)) AS lastdate
+		FROM
+		$wpdb->posts
+		WHERE
+		post_status = 'publish'
+		");
+	$output = '';
+	if($copyright_dates) {
+	$copyright = "&copy; " . $copyright_dates[0]->firstdate;
+	if($copyright_dates[0]->firstdate != $copyright_dates[0]->lastdate) {
+	$copyright .= '-' . $copyright_dates[0]->lastdate;
+}
+	$output = $copyright;
+}
+	return $output;
+}
+
+// Add "read more" to post excerpts
+function new_excerpt_more( $more ) {
+	return '<div align="right"> <a class="readmore" href="'. get_permalink( get_the_ID() ) . '"> Read More</a></div>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
+
+// Enable shortcodes in widgets
+add_filter('widget_text', 'do_shortcode');
+
+?>
